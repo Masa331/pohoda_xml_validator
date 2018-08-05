8e3c28c49644f1185b7889008e28df4007f8c07f
Premysl Donat <pdonat@seznam.cz>
2018-08-05 20:55:28 +0200

    Initial commit

--- spec/pohoda_xml_validator_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe PohodaXmlValidator do
-  it "has a version number" do
-    expect(PohodaXmlValidator::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
