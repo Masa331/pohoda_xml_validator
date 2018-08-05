ae5ff753e3503cefda5665a3daf92e2c31ccd33b
Premysl Donat <pdonat@seznam.cz>
2018-08-05 15:22:11 +0200

    Bundler gem

---
+++ lib/pohoda_xml_validator.rb
@@ -0,0 +1,5 @@
+require "pohoda_xml_validator/version"
+
+module PohodaXmlValidator
+  # Your code goes here...
+end
---
+++ spec/pohoda_xml_validator_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe PohodaXmlValidator do
+  it "has a version number" do
+    expect(PohodaXmlValidator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
